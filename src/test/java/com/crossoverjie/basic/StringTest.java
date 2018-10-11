fa2ed36c42822076dddea39a5f7c8285cf694c64
zhangjianmin@kuaicheyuan.com <zhangjianmin@kuaicheyuan.com>
2018-10-11 17:49:59 +0800

    增加格式化

--- src/test/java/com/crossoverjie/basic/StringTest.java
+++ src/test/java/com/crossoverjie/basic/StringTest.java
@@ -3,7 +3,6 @@ package com.crossoverjie.basic;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class StringTest {
     @Test
     public void StringEqualsTest() {
@@ -26,5 +25,19 @@ public class StringTest {
 
     }
 
+    @Test
+    public void StringFormatTest(){
+        int source = 1234;
+        String target = "+001234";
+        // 注意+也包括在长度内
+        String actual = String.format("%+07d",source);
+        System.out.println(actual);
+        Assert.assertTrue("字符串相等",target.equals(actual));
 
+        float sourceF = 1234.567F;
+        String targetF = "1,234.57";
+        String actualF = String.format("%,.2f",sourceF);
+        System.out.println(actualF);
+        Assert.assertTrue("字符串相等",targetF.equals(actualF));
+    }
 }
