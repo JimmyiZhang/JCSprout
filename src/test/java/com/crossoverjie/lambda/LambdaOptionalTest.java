fdbc48217b109296e50a937e0f9a8874b6697ac7
jimmy <zjm281@163.com>
2019-01-24 21:58:57 +0800

    lambda

--- src/test/java/com/crossoverjie/lambda/LambdaOptionalTest.java
+++ src/test/java/com/crossoverjie/lambda/LambdaOptionalTest.java
@@ -3,14 +3,25 @@ package com.crossoverjie.lambda;
 import org.junit.Test;
 import org.springframework.util.Assert;
 
+import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public class LambdaOptionalTest {
     @Test
-    public void optionalTest() {
+    public void optionalOrElseTest() {
         String str = null;
         String out = Optional.ofNullable(str).orElse("");
 
         Assert.isTrue(out.equals(""), "orElse默认值");
     }
+
+    @Test
+    public void optionalPresentTest(){
+        String str = "good";
+        Optional op = Optional.of(str);
+        op.ifPresent( v->System.out.println(v));
+
+        Assert.isTrue(op.isPresent(),"empty optional");
+    }
+
 }
